71d374a8c771b6895b845242461e9d0ab665f1e1
arcsino <173187432+arcsino@users.noreply.github.com>
2025-08-29 02:06:08 +0900

    ordering by -created_at

--- backend/quizapp/accounts/admin.py
+++ backend/quizapp/accounts/admin.py
@@ -7,3 +7,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "email", "is_staff", "is_active", "id"]
     search_fields = ["username"]
     readonly_fields = ["id"]
+    ordering = ["username"]
